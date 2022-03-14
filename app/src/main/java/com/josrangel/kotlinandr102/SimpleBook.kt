a55b9d6104af4db5ca37f65be8cac67002383d4d
josrangel <josrangel@hotmail.com>
2022-03-13 23:59:02 -0600

    Modificacion de la clase modelo

--- app/src/main/java/com/josrangel/kotlinandr102/SimpleBook.kt
+++ app/src/main/java/com/josrangel/kotlinandr102/SimpleBook.kt
@@ -2,5 +2,6 @@ package com.josrangel.kotlinandr102
 //se define la clase modelo o POJO de lo que queremos obtener de nuestra respuesta
 data class SimpleBook(
     val bookName: String,
-    val maxPrice:String
+    val maxPrice: String,
+    val minimumPrice: String
 )
